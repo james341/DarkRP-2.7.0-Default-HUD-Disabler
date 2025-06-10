0d0868269bc619b5d1a3865c08a9cf7896571827
james341 <taine049@gmail.com>
2025-06-10 11:34:10 +0800

    Update disabledefault_scoreboard.lua

--- lua/autorun/disabledefault_scoreboard.lua
+++ lua/autorun/disabledefault_scoreboard.lua
@@ -1,1 +1,10 @@
+-- Disable Default DarkRP Tab Menu (Scoreboard)
+hook.Add("ScoreboardShow", "DisableDarkRPScoreboardShow", function()
+    -- Return true to override default behavior
+    return true
+end)
 
+hook.Add("ScoreboardHide", "DisableDarkRPScoreboardHide", function()
+    -- Return true to suppress closing behavior
+    return true
+end)
